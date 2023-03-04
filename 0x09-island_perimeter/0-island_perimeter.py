225a4c7c5373be250cbb23465fd6bec2e43da648
Ayem-Dan <danielMMomodu@gmail.com>
2023-03-04 20:18:52 +0100

    Update

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,8 +4,7 @@ def island_perimeter(grid):
     Calculate the perimeter of the island described in grid.
 
     Parameters:
-    grid (List[List[int]]): A rectangular grid of 0s and 1s, where 0\
-          represents water and 1 represents land.
+    grid (List[List[int]]): A rectangular grid of 0s and 1s, where 0 represents water and 1 represents land.
 
     Returns:
     int: The perimeter of the island.
@@ -13,27 +12,19 @@ def island_perimeter(grid):
     # Define the directions for checking adjacent cells.
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-    # Initialize the perimeter and visited sets.
+    # Initialize the perimeter variable.
     perimeter = 0
-    visited = set()
 
     # Iterate through the grid to find the island.
     for row in range(len(grid)):
         for col in range(len(grid[0])):
-            if grid[row][col] == 1 and (row, col) not in visited:
-                # Use depth-first search to find the perimeter of the island.
-                stack = [(row, col)]
-                visited.add((row, col))
-                while stack:
-                    r, c = stack.pop()
-                    for dr, dc in directions:
-                        nr, nc = r + dr, c + dc
-                        if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
-                            if grid[nr][nc] == 0:
-                                perimeter += 1
-                            elif (nr, nc) not in visited:
-                                stack.append((nr, nc))
-                                visited.add((nr, nc))
-                        else:
-                            perimeter += 1
+            if grid[row][col] == 1:
+                # Add 4 to the perimeter for each land cell.
+                perimeter += 4
+                # Subtract 2 for each adjacent land cell.
+                for dr, dc in directions:
+                    nr, nc = row + dr, col + dc
+                    if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == 1:
+                        perimeter -= 2
+  
     return perimeter
