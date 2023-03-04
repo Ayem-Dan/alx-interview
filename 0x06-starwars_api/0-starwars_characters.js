0ca4b7ca2cb2e97a988f30cc2c9477f49446386d
Ayem-Dan <danielMMomodu@gmail.com>
2023-03-04 20:50:37 +0100

    Update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,58 @@
 #!/usr/bin/node
-const util = require('util');
-const request = util.promisify(require('request'));
-const filmID = process.argv[2];
+const https = require('https');
 
-async function getCharacters (filmId) {
-  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-  let response = await (await request(endpoint)).body;
-  response = JSON.parse(response);
-  const characters = response.characters;
+// Define the Star Wars API base URL
+const BASE_URL = "https://swapi.dev/api";
 
-  for (let i = 0; i < characters.length; i++) {
-    const urlCharacter = characters[i];
-    let character = await (await request(urlCharacter)).body;
-    character = JSON.parse(character);
-    console.log(character.name);
-  }
+// Parse the Movie ID from the command-line arguments
+if (process.argv.length < 3) {
+    console.error("Usage: node script.js <Movie ID>");
+    process.exit(1);
 }
 
-getCharacters(filmID);
+const movie_id = parseInt(process.argv[2]);
+
+// Fetch the movie data from the Star Wars API
+https.get(`${BASE_URL}/films/${movie_id}`, (res) => {
+    let data = "";
+
+    res.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    res.on('end', () => {
+        if (res.statusCode !== 200) {
+            console.error("Error: Could not fetch movie data");
+            process.exit(1);
+        }
+
+        const movie_data = JSON.parse(data);
+
+        // Fetch the character data for the movie
+        const characters = movie_data.characters;
+
+        characters.forEach((character_url) => {
+            https.get(character_url, (res) => {
+                let data = "";
+
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+                res.on('end', () => {
+                    if (res.statusCode !== 200) {
+                        console.error("Error: Could not fetch character data");
+                        process.exit(1);
+                    }
+
+                    const character_data = JSON.parse(data);
+                    console.log(character_data.name);
+                });
+            }).on('error', (err) => {
+                console.error(err);
+            });
+        });
+    });
+}).on('error', (err) => {
+    console.error(err);
+});
